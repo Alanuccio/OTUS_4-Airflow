9de3882b109603c12d48cf3deae7ee6ada8b7542
AlanDeSemia <aae.sem@gmail.com>
2021-07-12 21:55:10 +0600

    first draft to commit

---
+++ draft1.py
@@ -0,0 +1,32 @@
+from time import time
+import psycopg2
+
+
+def main():
+    conn = psycopg2.connect("""
+        host=rc1c-t29vq5k95bcqks94.mdb.yandexcloud.net
+        port=6432
+        dbname=db_alanuccio
+        user=katakato
+        password=<password>
+        target_session_attrs=read-write
+        sslmode=verify-full
+    """)
+
+    print(conn.version)
+# End main
+
+
+if __name__ == '__main__':
+    start_time = time()
+    delimiter = '- ' * 20 + '-'
+    try:
+        print(delimiter)
+        main()
+    except Exception as e:
+        raise e
+    finally:
+        print(delimiter)
+        print('Script took {:.5f} seconds'.format((time() - start_time)))
+        print(delimiter)
+# End
