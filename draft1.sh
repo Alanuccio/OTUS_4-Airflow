0cc51912f04d947b34dd59dd23d3ad4f5f406197
airflow1 <aae.sem@yandex.ru>
2021-07-14 16:29:18 +0000

    updated in cloud

--- draft1.sh
+++ draft1.sh
@@ -1,1 +1,1 @@
-PGPASSWORD=$PSQL_PASSWORD psql -h $PSQL_HOST -p 6432 -d $PSQL_DBNAME -U $PSQL_USER -q -c "SELECT * FROM bitcoin_course" -t
+PGPASSWORD=$PSQL_PASSWORD psql -h $PSQL_HOST -p 6432 -d $PSQL_DBNAME -U $PSQL_USER -q -c "SELECT * FROM bitcoin_course" -t -P format=unaligned
