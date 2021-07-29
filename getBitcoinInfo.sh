7bbc0d372c6b501c03d2c2f5a672900931978b2a
katakato <aae.sem@yandex.ru>
2021-07-29 09:09:48 +0000

    adding prod bash script

---
+++ getBitcoinInfo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+json_inc=$(curl --location --request GET 'api.coincap.io/v2/rates/bitcoin');
+
+coincap_id=$(echo "$json_inc" | jq -r ".data.id");
+coincap_symbol=$(echo "$json_inc" | jq -r ".data.symbol");
+coincap_currentSymbol=$(echo "$json_inc" | jq -r ".data.currencySymbol");
+coincap_type=$(echo "$json_inc" | jq -r ".data.type");
+coincap_rateUsd=$(echo "$json_inc" | jq -r ".data.rateUsd");
+coincap_timestamp=$(echo "$json_inc" | jq -r ".timestamp");
+
+# echo {$coincap_id,$coincap_rateUsd,$coincap_timestamp};
+
+query_ins="INSERT INTO public.bitcoin_course (id, symbol, currencysymbol, type, rateusd, timestamp) VALUES ('$coincap_id', '$coincap_symbol', '$coincap_currentSymbol', '$coincap_type', '$coincap_rateUsd', $coincap_timestamp);"
+
+# echo $query_ins;
+
+PGPASSWORD=$PSQL_PASSWORD psql -h $PSQL_HOST -p 6432 -d $PSQL_DBNAME -U $PSQL_USER -q -c "$query_ins";
+
+echo {$PSQL_HOST,$PSQL_DBNAME};
+
+echo "Done";
